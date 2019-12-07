1bff191a4122c01aad8049131e8337b9ed868049
Gabrielle Umbach <g.umbach8098@gmail.com>
2019-12-07 10:07:23 -0600

    setting up files and organizing them.

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,9 @@
+let path = require("path");
+	module.exports = function (app) {
+	    app.get(`/`, (req, res) => {
+	        res.sendFile(path.join(__dirname, `../public/home.html`));
+	    });
+	    app.get(`/survey`, (req, res) => {
+	        res.sendFile(path.join(__dirname, `../public/survey.html`));
+	    });
+	};
---
+++ server.js
@@ -0,0 +1,10 @@
+let express = require(`express`);
+	let app = express();
+	let PORT = process.env.PORT || 8080;
+	app.use(express.urlencoded({ extended: true }));
+	app.use(express.json());
+	require("./app/routing/apiRoutes")(app);
+	require("./app/routing/htmlRoutes")(app);
+	app.listen(PORT, () => {
+	    console.log(`App Listening on port: ${ PORT }`);
+	});
